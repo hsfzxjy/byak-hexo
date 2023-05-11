6b6441ff6ca66c2bfcbcfc69977b722f4fc0c3a9
hsfzxjy <hsfzxjy@gmail.com>
2023-05-12 01:10:22 +0800

    Add d2 executor

--- scripts/genko-markdown.js
+++ scripts/genko-markdown.js
@@ -24,6 +24,29 @@ edge [color="#444444", fontcolor="#444444", constraint=false, fontsize=10];
         : genko.util.render.asTextBlock(result.toString())
     },
   })
+
+  genko.codex.Executor.register("d2", {
+    run: async (text, options) => {
+      let args = ["--pad", "0"]
+      args.push("--bundle=false")
+      if (options.args.theme) {
+        args.push("--theme", options.args.theme)
+      }
+      if (options.args.sketch) {
+        args.push("--sketch")
+      }
+
+      args = [
+        "-c",
+        `svgo -p 0 <(d2 ${args.join(" ")} {{input}} -) -o {{output}}`,
+      ]
+      const result = await genko.util.exec("/bin/bash", args, { stdin: text })
+
+      return result.isSuccess
+        ? `<figure class="gk-d2">${result.output}</figure>`
+        : genko.util.render.asTextBlock(result.toString())
+    },
+  })
 }
 
 hexo.extend.renderer.register("md", "html", async (data) => {
