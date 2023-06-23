4647bd8404cdb4a8d868b6f3b5282b89bc8f9124
hsfzxjy <hsfzxjy@gmail.com>
2023-06-23 16:03:47 +0800

    Bump mathjax to 4.0.0 alpha. Enable line breaking.

--- source/_js/src/mathjax.ts
+++ source/_js/src/mathjax.ts
@@ -7,27 +7,32 @@ window.MathJax = {
     },
     packages: { "[+]": ["noerrors"] },
   },
-  "HTML-CSS": {
+
+  // See https://github.com/mathjax/MathJax-src/releases/tag/4.0.0-alpha.1#linebreaking
+  output: {
+    displayOverflow: "linebreak",
     linebreaks: {
-      automatic: true,
-      width: "container",
+      // options for when overflow is linebreak
+      inline: true, // true for browser-based breaking of inline equations
+      width: "100%", // a fixed size or a percentage of the container width
+      lineleading: 0, // the default lineleading in em units
+      LinebreakVisitor: null, // The LinebreakVisitor to use
     },
   },
+
   SVG: {
     fontCache: "global",
-    linebreaks: { automatic: true },
   },
   options: {
     ignoreHtmlClass: "tex2jax_ignore",
     processHtmlClass: "tex2jax_process",
   },
-  loader: {
-    load: ["[tex]/noerrors"],
-  },
+  loader: { load: ["input/tex", "output/chtml"] },
 }
 document.addEventListener("DOMContentLoaded", function () {
   const script = document.createElement("script")
-  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-svg.js"
+  script.src =
+    "https://cdn.jsdelivr.net/npm/mathjax@4.0.0-alpha.1/es5/tex-svg.js"
   script.async = true
   script.defer = true
   document.body.appendChild(script)
